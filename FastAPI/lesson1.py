9b5d6c00eb5b98392a041615f8ca5129947e28ed
KedarDamale <damalekedar@gmail.com>
2025-06-25 20:23:48 +0530

    Added notes

---
+++ FastAPI/lesson1.py
@@ -0,0 +1,18 @@
+"""
+FastAPI is a modern, fast (high-performance), 
+web framework for building APIs with Python 3.7+,
+based on standard Python type hints. 
+It is designed for building RESTful APIs 
+and is known for being extremely fast, 
+efficient, and easy to use.
+
+It is Built on Starlette (for web handling) and Pydantic (for data validation)
+
+Provides full support for async/await, making it excellent for high-concurrency applications
+
+Uses Python type hints for automatic validation and editor support
+
+Automatically generates interactive API docs using Swagger UI and ReDoc
+
+"""
+
